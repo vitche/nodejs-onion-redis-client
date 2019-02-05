46a2865c1036105fd78d1e86d1570ebb5c837a7e
Vitche Research Team Developer <developer@vitche.com>
2019-02-05 17:38:57 +0000

    1. Среда последовательного выполнения тестов. 2. TCP-тоннель для GRPC. 3. GRPC-клиент OnionRedis.

--- main.js
+++ main.js
@@ -1,27 +1,59 @@
-var HTTPAgent = require('socks-proxy-agent');
-var webSocket = require('socket.io-client');
-var grpcSocket = require('nodejs-grpc-socketio-proxy');
-module.exports = function (uri, proxyAddress, transport) {
+const url = require('url');
+const HTTPAgent = require('socks-proxy-agent');
+const webSocket = require('socket.io-client');
+const grpcSocket = require('nodejs-grpc-socketio-proxy');
+const tunnelFactory = require('nodejs-proxy-tcp-tunnel');
+module.exports = function (serverAddress, proxyAddress) {
 	var self = this;
-	if (!transport) {
-		transport = 'grpc';
+	var proxyUrl = undefined;
+	if (proxyAddress) {
+		proxyUrl = url.parse(proxyAddress);
+	}
+	var serverUrl = undefined;
+	if (serverAddress) {
+		serverUrl = url.parse(serverAddress);
 	}
 	// Connects to the Onion Redis queue
 	this.connect = function (callback) {
 		var socket;
 		var configuration = {};
-		if (proxyAddress) {
-			configuration.agent = new HTTPAgent(proxyAddress);
+		if ('grpc:' === serverUrl.protocol) {
+			if (proxyAddress) {
+				var localAddress = '127.0.0.1';
+				var localPort = parseInt(serverUrl.port) + 8000;
+				self.tunnel = tunnelFactory
+					.from(localAddress, localPort)
+					.through('socks5', proxyUrl.hostname, proxyUrl.port)
+					.to(serverUrl.hostname, serverUrl.port)
+					.start();
+				// Change server address to the tunnel
+				serverAddress = localAddress + ':' + localPort;
+			}
+			socket = grpcSocket.ClientSocket;
+		} else if ('socketio' === transport) {
+			if (proxyAddress) {
+				configuration.agent = new HTTPAgent(proxyAddress);
+			}
+			socket = webSocket;
 		}
-		socket = 'grpc' === transport ? grpcSocket.ClientSocket : webSocket;
-		self.connection = socket.connect(uri, configuration);
+		self.connection = socket.connect(serverAddress, configuration);
 		self.connection.on('connect', function () {
-			callback();
+			// Let the tunnel to be initialized
+			setTimeout(function () {
+				callback();
+			}, 3000);
 		});
 	};
-	this.disconnect = function () {
-		if (undefined !== self.connection) {
+	this.disconnect = function (callback) {
+		if (self.connection) {
 			self.connection.disconnect();
+			self.connection = undefined;
+		}
+		if (self.tunnel) {
+			self.tunnel.close(callback);
+			self.tunnel = undefined;
+		} else {
+			callback();
 		}
 	};
 	this.publish = function (channel, message) {
--- tests/queue.js
+++ tests/queue.js
@@ -2,13 +2,14 @@ module.exports = {
 	testPublish: function (test) {
 		var configuration = require('../configuration');
 		var OnionRedisClient = require("..");
-		var publisher = new OnionRedisClient(configuration.namespace, configuration.proxy.address, configuration.transport);
+		var publisher = new OnionRedisClient(configuration.namespace, configuration.proxy.address);
 		publisher.connect(function (error) {
 			if (undefined !== error) {
 				console.log(error);
 				return;
 			}
 			publisher.on('data', function (data) {
+				test.ok(true, 'should publish data')
 				test.done();
 			});
 			publisher.publish('test', ':)');
@@ -17,8 +18,8 @@ module.exports = {
 	testSubscribe: function (test) {
 		var configuration = require('../configuration');
 		var OnionRedisClient = require("..");
-		var listener = new OnionRedisClient(configuration.namespace, configuration.proxy.address, configuration.transport);
-		var publisher = new OnionRedisClient(configuration.namespace, configuration.proxy.address, configuration.transport);
+		var listener = new OnionRedisClient(configuration.namespace, configuration.proxy.address);
+		var publisher = new OnionRedisClient(configuration.namespace, configuration.proxy.address);
 		listener.connect(function (error) {
 			var subscribed = false;
 			if (undefined !== error) {
@@ -39,6 +40,21 @@ module.exports = {
 					return;
 				}
 				publisher.publish('test', ':)');
+			});
+		});
+	},
+	testDisconnect: function (test) {
+		var configuration = require('../configuration');
+		var OnionRedisClient = require("..");
+		var publisher = new OnionRedisClient(configuration.namespace, configuration.proxy.address);
+		publisher.connect(function (error) {
+			if (undefined !== error) {
+				console.log(error);
+				return;
+			}
+			publisher.disconnect(function () {
+				test.ok(true, 'should terminate the tunnel')
+				test.done();
 			});
 		});
 	}
